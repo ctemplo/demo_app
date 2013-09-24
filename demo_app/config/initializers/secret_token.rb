ef564c51b62e297e23073c3ce79fd7a8d9b0b32c
Celso Templo <ctemplo@gmail.com>
2013-09-24 13:12:45 -0700

    Revert "Initial commit"
    This reverts commit 658030de69b6823a201f00f9392b7cd53ace6328.

--- demo_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'f3292afd693c2c7630ac87e1c01f6b34d3523ca986e9b82609e16ef2f8d29b9a3f875bf0a5c2999c0c658052a2025c1ccf11b0dbcbfc3e0ca14f3857a5841596'
